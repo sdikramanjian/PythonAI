17c1e789fb2726af12d5cef96d27c5c7d84bfba6
sdikramanjian <mari.varna49@gmail.com>
2023-11-08 22:43:12 -0500

    Stub for the function to return recommendations

---
+++ advisor.py
@@ -0,0 +1,7 @@
+class Advisor:
+    def __init__(self, name):
+        self.name = name
+
+    def recommend_stocks(self, investor_type):
+        # TODO: Implement stock recommendation logic based on investor type
+        return ['AAPL', 'GOOG', 'MSFT']
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-from bottle import route, run, template, TEMPLATE_PATH
+from bottle import route, request,run, template, TEMPLATE_PATH
+from advisor import Advisor
 #import requests
 
 TEMPLATE_PATH.append('./ui_templates/') # Set the path to the directory containing my html templates
 
 investor_types = ['Conservative', 'Moderate', 'Aggressive'] # Create an array called investor_types
-result = '' # Create a variable called result
+result = '' # initiate a variable called result
+my_name = 'Susan' # initiate a variable called myName
 
 @route('/')
 def index():
-    return template('top_level_component.tpl', title='Welcome', name='Susan', investor_types=investor_types, result=result) 
+    return template('top_level_component.tpl', title='Welcome', name=my_name, investor_types=investor_types, result=result) 
 
 @route('/',method='POST')
 def submit_form():
-   # investor_type=request.forms.get('investor_type')
-    return template('top_level_component.tpl', title='Welcome', name='Susan', investor_types=investor_types, result=f"Here are some stocks we want to recomment to you:")
+    investor_type=request.forms.get('investor_type')
+    return template('top_level_component.tpl', title='Welcome', name=my_name, investor_types=investor_types, result=f"Here are some stocks we want to recomment to you {my_name} since you are a {investor_type} investor: {Advisor(my_name).recommend_stocks(investor_type)}")
 
 if __name__ =='__main__':
     run(host='localhost',port=8080,debug=True)
