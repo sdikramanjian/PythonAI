15663c89df4cccfaa3919bce86b1ee57ab9ceda5
sdikramanjian <mari.varna49@gmail.com>
2023-11-12 18:54:54 -0500

    Managed to get the data for 1 stock - bulk next

--- advisor.py
+++ advisor.py
@@ -1,7 +1,10 @@
-class Advisor:
-    def __init__(self, name):
-        self.name = name
+import yfinance as yf
+from pandas_datareader import data as pdr
 
-    def recommend_stocks(self, investor_type):
-        # TODO: Implement stock recommendation logic based on investor type
+class Advisor:
+    def __init__(self):
+        yf.pdr_override()
+
+    def recommend_stocks(self,investor_type):
+        data = pdr.get_data_yahoo("AAPL", start="2017-01-01", end="2023-09-30") # from Jan 1 2017 until Sept 30 2023
         return ['AAPL', 'GOOG', 'MSFT']
