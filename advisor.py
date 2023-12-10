138b5fc9caa42ba90806d8a4873b6cb07db73e9d
sdikramanjian <mari.varna49@gmail.com>
2023-12-10 12:45:53 -0500

    Works mechanically - more edits needed

--- advisor.py
+++ advisor.py
@@ -1,10 +1,22 @@
 import yfinance as yf
+import pandas as pd
 from pandas_datareader import data as pdr
+
 
 class Advisor:
     def __init__(self):
         yf.pdr_override()
 
     def recommend_stocks(self,investor_type):
-        data = pdr.get_data_yahoo("AAPL", start="2017-01-01", end="2023-09-30") # from Jan 1 2017 until Sept 30 2023
-        return ['AAPL', 'GOOG', 'MSFT']
+        stocks_list= self.get_list_of_tickers()[:100] # get the top 1000 stocks (for performance reasons)
+        string_stocks_list = [str(stock) for stock in stocks_list]
+        stock_data = pdr.get_data_yahoo(string_stocks_list, start="2023-07-01", end="2023-11-30") # from July 1 2023 until Nov 30 2023
+        return stocks_list[:10] #placeholder until the logic is done
+    
+    
+    def get_list_of_tickers(self):
+        csv_file_path = 'data_sets/nasdaq_data_set.csv' # Specify the path to the nasdaq dataset file
+        df = pd.read_csv(csv_file_path) # Read the CSV file into a pandas DataFrame
+        ticker_values = df.iloc[:, 0].tolist() # Extract only the ticker symbols
+        return ticker_values # Return the list of ticker symbols
+      
