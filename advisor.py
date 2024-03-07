a9c5092563797a0ad9650361d7afe8a6998e8021
sdikramanjian <mari.varna49@gmail.com>
2024-03-07 12:32:33 -0500

    Comitting my atest Changes

--- advisor.py
+++ advisor.py
@@ -10,13 +10,13 @@ class Advisor:
     def recommend_stocks(self,investor_type):
         stocks_list= self.get_list_of_tickers()[:100] # get the top 1000 stocks (for performance reasons)
         string_stocks_list = [str(stock) for stock in stocks_list]
-        stock_data = pdr.get_data_yahoo(string_stocks_list, start="2023-07-01", end="2023-11-30") # from July 1 2023 until Nov 30 2023
+        #stock_data = pdr.get_data_yahoo(string_stocks_list, start="2023-07-01", end="2023-11-30") # from July 1 2023 until Nov 30 2023
         return stocks_list[:10] #placeholder until the logic is done
     
     
     def get_list_of_tickers(self):
         csv_file_path = 'data_sets/nasdaq_data_set.csv' # Specify the path to the nasdaq dataset file
         df = pd.read_csv(csv_file_path) # Read the CSV file into a pandas DataFrame
-        ticker_values = df.iloc[:, 0].tolist() # Extract only the ticker symbols
+        ticker_values = df.iloc[:, 1].tolist() # Extract only the Company name
         return ticker_values # Return the list of ticker symbols
       
